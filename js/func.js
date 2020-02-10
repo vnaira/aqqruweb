4eb95ef016caf47c10dfa49861227e7321a83c96
vnairav <vnairav@apollon.de>
2020-02-10 18:54:20 +0400

    personal information multi select answer

--- js/func.js
+++ js/func.js
@@ -2,13 +2,52 @@ $(document).ready(function () {
     "use strict";
 
     $(".sent_msg").on('click', function () {
+        console.log(count($('.msg_history').find('.active')));
+        $('.msg_history').find('.active').removeAttribute('class','active');
         $(this).parent().find('.edit_delete').toggleClass('active');
     });
 
-    $(".answer-btn").on('click', function () {
+    $("#single-select .answer-btn").on('click', function () {
+        $('#single-select').find('.active').removeClass('active');
         $('#outgoing_value').val();
         $('#outgoing_value').val($(this).text());
+        if ($('#outgoing_value').val() == $(this).text()) {
+            $(this).addClass('active');
+        }
+    });
+    
+    $("#multiselect .answer-btn").on('click', function () {
+        let answer = $('#outgoing_value').val();
+        $(this).toggleClass('active');
 
+        if ($(this).hasClass('active')) {
+            if (answer !== '') {
+                if ($(this).text() == 'Other:') {
+                    $('#outgoing_value').focus();
+                    answer += ',';
+                } else
+                    answer += ',' + $(this).text();
+            } else {
+                if ($(this).text() == 'Other:') {
+                    $('#outgoing_value').focus();
+                } else
+                    answer += $(this).text();
+            }
+        } else {
+            let currentVal = ($('#outgoing_value').val()).split(',');
+            var result = arrayRemove(currentVal, $(this).text());
+            answer = result.join(',');
+        }
+
+        $('#outgoing_value').val(answer);
     });
 
+
+    function arrayRemove(arr, value) {
+        return arr.filter(function(ele){
+            return ele != value;
+        });
+    }
 });
+
+
